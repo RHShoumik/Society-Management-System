3c0303af13eb30cf6ad266680e05eec0dc6af5e8
rhshoumik <rhshoumik12@gmail.com>
2020-04-03 23:08:55 +0600

    I am shoumik upload my(registration page) and erin(login Page) work

---
+++ createAccount.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/createAccount.css">
+
+</head>
+<body>  
+  
+
+    <div class="registration">
+       <p>Create an Account</p>
+       <form action="">
+
+              <div class="input-field">
+                <input class="textfield" type="text" placeholder="Name">
+              </div>
+              <div class="input-field">
+                <input class="textfield" type="text" placeholder="Email">
+              </div>
+              <div class="input-field">
+                <input class="textfield" type="text" placeholder="Password">
+              </div>
+              <div class="input-field">
+                <input class="textfield" type="text" placeholder="Re-Password">
+              </div>
+              <div class="input-field">
+                <div class="signup-btn">
+                  <a href="#">Sign In</a>
+                </div>  
+              </div>
+              
+              <a href="#">Already have one?</a>
+          </div>
+
+       </form>
+        
+    </div>
+    
+
+
+    
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Society Management System</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/stylelogin.css">
+
+<body>
+
+     <img src ="assets/image/loginBackground.jpg"class="log">
+    <div class="loginbox">
+    <img src="assets/image/login1.png" class="LogIn1">
+
+    <h1>Login Here</h1>
+    
+    <form>
+    <p> Username</p>
+    <input type="text" name="" placeholder="Enter Username">
+    <p> Password</p>
+    <input type="password" name="" placeholder="Enter Password"><br>
+    <input type="submit" name="" value="Login">
+    <a href="#">Lost your password?</a><br>
+    <a href="#">Don't have an acoount?</a>
+    </form> 
+    </div>
+  
+</body>
+</head>
+</html>
